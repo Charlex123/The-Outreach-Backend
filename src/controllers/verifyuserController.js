4973f35519b904b358220925986e7c18e073316d
2036380 <charlesmuoka1@gmail.com>
2023-08-17 11:46:19 +0100

    another commit

--- src/controllers/verifyuserController.js
+++ src/controllers/verifyuserController.js
@@ -40,4 +40,40 @@ const verifyUser = asyncHandler(async (req, res) => {
 });
 
 
-module.exports = { verifyUser };
+const verifyUserData = asyncHandler(async (req, res) => {
+  try {
+      
+      const user_appkey = req.body.userappkey;
+
+      const verifyuserdata = await User.findOne({userAppKey: user_appkey});
+
+      if (verifyuserdata) {
+        verifyuserdata.verified = true;
+        
+        const verifiedUser = await verifyuserdata.save();
+        const _id = verifiedUser._id;
+        const googleId = verifiedUser.googleId;
+        const email = verifiedUser.email;
+        const userAppKey = verifiedUser.userAppKey;
+        const accessToken = verifiedUser.accessToken;
+
+          res.json({
+            _id: verifiedUser._id,
+            googleId: verifiedUser.googleId,
+            email: verifiedUser.email,
+            accessToken: verifiedUser.accessToken,
+            userAppKey: verifiedUser.userAppKey
+          });
+        } else {
+          res.status(404);
+          throw new Error("User Not Found");
+        }
+  }catch (error) {
+    console.log('server error',error);
+    // res.status(500).json({ message: error.message });
+  }
+  
+});
+
+
+module.exports = { verifyUser, verifyUserData };
